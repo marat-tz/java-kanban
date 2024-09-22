192868664ff223d547966a3d2a2ff34b6d758161
Marat Tazhetdinov <unlim089@yandex.ru>
2024-09-22 19:08:14 +0300

    Add exception ManagerSaveException

---
+++ src/exceptions/ManagerSaveException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ManagerSaveException extends Exception {
+    public ManagerSaveException() {
+    }
+
+    public ManagerSaveException(final String message) {
+        super(message);
+    }
+}
