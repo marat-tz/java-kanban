d96a7560715cade004136e9a2f7e950544cb3d25
Marat Tazhetdinov <unlim089@yandex.ru>
2024-09-22 16:11:38 +0300

    Add class FileBackedTaskManager

---
+++ src/managers/FileBackedTaskManager.java
@@ -0,0 +1,93 @@
+package managers;
+
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.Task;
+
+import java.io.File;
+
+public class FileBackedTaskManager extends InMemoryTaskManager {
+
+    private File file;
+
+    public FileBackedTaskManager(File file) {
+        this.file = file;
+    }
+
+    private void save() {
+
+    }
+
+    @Override
+    public Task addNewTask(Task newTask) {
+        Task task = super.addNewTask(newTask);
+        save();
+        return task;
+    }
+
+    @Override
+    public Epic addNewTask(Epic newEpic) {
+        Epic epic = super.addNewTask(newEpic);
+        save();
+        return epic;
+    }
+
+    @Override
+    public Subtask addNewTask(Subtask newSubtask) {
+        Subtask subtask = super.addNewTask(newSubtask);
+        save();
+        return subtask;
+    }
+
+    @Override
+    public Task updateTask(Task updatedTask) {
+        Task task = super.updateTask(updatedTask);
+        save();
+        return task;
+    }
+
+    @Override
+    public Subtask updateTask(Subtask subtaskUpdate) {
+        Subtask subtask = super.updateTask(subtaskUpdate);
+        save();
+        return subtask;
+    }
+
+    @Override
+    public Epic updateTask(Epic epicUpdate) {
+        Epic epic = super.updateTask(epicUpdate);
+        save();
+        return epic;
+    }
+
+    @Override
+    public Task deleteTask(Integer taskId) {
+        Task task =  super.deleteTask(taskId);
+        save();
+        return task;
+    }
+
+    @Override
+    public void deleteAllTasks() {
+        super.deleteAllTasks();
+        save();
+    }
+
+    @Override
+    public void deleteAllSubtasks() {
+        super.deleteAllSubtasks();
+        save();
+    }
+
+    @Override
+    public void deleteAllEpic() {
+        super.deleteAllEpic();
+        save();
+    }
+
+    @Override
+    public void deleteEpicSubtasks(Integer epicId) {
+        super.deleteEpicSubtasks(epicId);
+        save();
+    }
+}
