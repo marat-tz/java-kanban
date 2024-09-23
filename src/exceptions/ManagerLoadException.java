8d7dcb83f6e60cf5208055e2031743b2eb597795
Marat Tazhetdinov <unlim089@yandex.ru>
2024-09-23 19:47:15 +0300

    Add ManagerLoadException

---
+++ src/exceptions/ManagerLoadException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ManagerLoadException extends Exception {
+    public ManagerLoadException() {
+    }
+
+    public ManagerLoadException(final String message) {
+        super(message);
+    }
+}
