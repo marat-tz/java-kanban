ffa25cc7ef1675e55b6359575f8ebfcac12dd327
Marat Tazhetdinov <unlim089@yandex.ru>
2024-09-23 19:54:22 +0300

    Add name, description fields in equals method

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -81,7 +81,7 @@ public class Task {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return id == task.id;
+        return id == task.id && name == task.name && description == task.description;
     }
 
     @Override
