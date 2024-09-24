efef9c4cafb87510b4368c28f7f9a7a72e8defa7
Marat Tazhetdinov <unlim089@yandex.ru>
2024-09-24 18:28:55 +0300

    Remove unused constructor

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -1,8 +1,6 @@
 package exceptions;
 
 public class ManagerSaveException extends Exception {
-    public ManagerSaveException() {
-    }
 
     public ManagerSaveException(final String message) {
         super(message);
