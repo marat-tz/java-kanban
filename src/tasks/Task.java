07a1159c772a48ef49b12a83287e8e5e87bd1da4
Marat Tazhetdinov <unlim089@yandex.ru>
2024-10-06 17:24:34 +0300

    Add duration and startTime in toString

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -107,7 +107,7 @@ public class Task {
 
     @Override
     public String toString() {
-        return String.format("%s,%s,%s,%s,%s", id, type, name, status, description);
+        return String.format("%s,%s,%s,%s,%s,%s,%s", id, type, name, status, description, duration, startTime);
     }
 
 }
