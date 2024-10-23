66c7ae7e8e36ed235cedd0561850ee3467a3d575
Marat Tazhetdinov <unlim089@yandex.ru>
2024-10-23 20:49:17 +0300

    Add try-catch in BaseHttpHandler sendText().

--- src/handlers/BaseHttpHandler.java
+++ src/handlers/BaseHttpHandler.java
@@ -3,7 +3,6 @@ package handlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,15 +10,16 @@ import java.util.logging.Logger;
 public abstract class BaseHttpHandler implements HttpHandler {
     private static final Logger logger = Logger.getAnonymousLogger();
 
-    protected void sendText(HttpExchange h, String text, Integer code) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+    protected void sendText(HttpExchange h, String text, Integer code) {
         try {
+            byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+            h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
             h.sendResponseHeaders(code, resp.length);
-        } catch (IOException io) {
-            logger.log(Level.SEVERE, "error while try to response headers", io);
+            h.getResponseBody().write(resp);
+            h.close();
+        } catch (Exception e) {
+            sendText(h, "internal server error", 500);
+            logger.log(Level.SEVERE, "error while send text", e);
         }
-        h.getResponseBody().write(resp);
-        h.close();
     }
 }
