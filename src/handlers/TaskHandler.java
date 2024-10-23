bce158dfe14a217f6f87eee5737001c382bc788f
Marat Tazhetdinov <unlim089@yandex.ru>
2024-10-23 20:56:28 +0300

    Add try-catch in TaskHandler methods. Add response 500

--- src/handlers/TaskHandler.java
+++ src/handlers/TaskHandler.java
@@ -9,16 +9,19 @@ import type.adapters.DurationAdapter;
 import type.adapters.LocalDateTimeAdapter;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 public class TaskHandler extends BaseHttpHandler {
+
+    private static final Logger logger = Logger.getAnonymousLogger();
 
     protected final TaskManager manager;
     protected final Gson gson = new GsonBuilder()
@@ -33,70 +36,91 @@ public class TaskHandler extends BaseHttpHandler {
 
     @Override
     public void handle(HttpExchange h) throws IOException {
-        InputStream inputStreamBody = h.getRequestBody();
-        String requestMethod = h.getRequestMethod();
-        String requestPath = h.getRequestURI().getPath();
-        String body = new String(inputStreamBody.readAllBytes(), StandardCharsets.UTF_8);
+        try {
+            String requestMethod = h.getRequestMethod();
+            String requestPath = h.getRequestURI().getPath();
+            String body = new String(h.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
 
-        if (Pattern.matches("/tasks/", requestPath) || Pattern.matches("/tasks", requestPath)) {
-            switch (requestMethod) {
-                case "GET":
-                    sendText(h, taskListSerialize(manager.getAllTasks()), 200);
-                    break;
-                case "POST":
-                    if (body.isEmpty() || body.isBlank()) {
-                        sendText(h, "request body is empty", 400);
-                    } else if (!body.contains("\"id\"")) {
-                        addTask(h, body);
-                    } else {
-                        updateTask(h, body);
-                    }
-                    break;
-            }
-
-        } else if (Pattern.matches("/tasks/\\d+", requestPath)
-                || Pattern.matches("/tasks/\\d+/", requestPath)) {
-            Optional<Integer> id = getId(requestPath);
-            if (id.isPresent()) {
+            if (Pattern.matches("/tasks/", requestPath) || Pattern.matches("/tasks", requestPath)) {
                 switch (requestMethod) {
-                    case "GET" -> getTask(h, id.get());
-                    case "DELETE" -> deleteTask(h, id.get());
+                    case "GET":
+                        sendText(h, taskListSerialize(manager.getAllTasks()), 200);
+                        break;
+                    case "POST":
+                        if (body.isEmpty() || body.isBlank()) {
+                            sendText(h, "request body is empty", 400);
+                        } else if (!body.contains("\"id\"")) {
+                            addTask(h, body);
+                        } else {
+                            updateTask(h, body);
+                        }
+                        break;
                 }
+
+            } else if (Pattern.matches("/tasks/\\d+", requestPath)
+                    || Pattern.matches("/tasks/\\d+/", requestPath)) {
+                Optional<Integer> id = getId(requestPath);
+                if (id.isPresent()) {
+                    switch (requestMethod) {
+                        case "GET" -> getTask(h, id.get());
+                        case "DELETE" -> deleteTask(h, id.get());
+                    }
+                }
+
+            } else {
+                sendText(h, "Unknown request", 404);
             }
 
-        } else {
-            sendText(h, "Unknown request", 404);
+        } catch (Exception e) {
+            sendText(h, "internal server error", 500);
+            logger.log(Level.SEVERE, "error while handle task request", e);
         }
     }
 
-    private void addTask(HttpExchange h, String body) throws IOException {
-        Task addedTask = manager.addNewTask(gson.fromJson(body, Task.class));
-        if (Objects.nonNull(addedTask)) {
-            sendText(h, taskSerialize(addedTask), 201);
-        } else {
-            sendText(h, "Task time overlaps with existing tasks", 406);
+    private void addTask(HttpExchange h, String body) {
+        try {
+            Task addedTask = manager.addNewTask(gson.fromJson(body, Task.class));
+            if (Objects.nonNull(addedTask)) {
+                sendText(h, taskSerialize(addedTask), 201);
+            } else {
+                sendText(h, "Task time overlaps with existing tasks", 406);
+            }
+        } catch (Exception e) {
+            sendText(h, "internal server error", 500);
+            logger.log(Level.SEVERE, "error while add task", e);
         }
     }
 
-    private void updateTask(HttpExchange h, String body) throws IOException {
-        Task updatedTask = manager.updateTask(gson.fromJson(body, Task.class));
-        if (Objects.nonNull(updatedTask)) {
-            sendText(h, taskSerialize(updatedTask), 201);
-        } else {
-            sendText(h, "Task time overlaps with existing tasks", 406);
+    private void updateTask(HttpExchange h, String body) {
+        try {
+            Task updatedTask = manager.updateTask(gson.fromJson(body, Task.class));
+            if (Objects.nonNull(updatedTask)) {
+                sendText(h, taskSerialize(updatedTask), 201);
+            } else {
+                sendText(h, "Task time overlaps with existing tasks", 406);
+            }
+        } catch (Exception e) {
+            sendText(h, "internal server error", 500);
+            logger.log(Level.SEVERE, "error while update task", e);
         }
     }
 
-    private void getTask(HttpExchange h, Integer taskId) throws IOException {
-        Task task = manager.getTask(taskId);
+    private void getTask(HttpExchange h, Integer taskId) {
+        try {
+            Task task = manager.getTask(taskId);
             if (Objects.isNull(task)) {
                 sendText(h, "Task with id " + taskId + " is not exist", 404);
             } else {
                 sendText(h, taskSerialize(task), 200);
             }
+        } catch (Exception e) {
+            sendText(h, "internal server error", 500);
+            logger.log(Level.SEVERE, "error while get task", e);
+        }
     }
 
-    private void deleteTask(HttpExchange h, Integer taskId) throws IOException {
+    private void deleteTask(HttpExchange h, Integer taskId) {
+        try {
             Task delTask = manager.deleteTask(taskId);
             if (Objects.nonNull(delTask)) {
                 String response = "Successful remove task: " + "id: "
@@ -105,6 +129,10 @@ public class TaskHandler extends BaseHttpHandler {
             } else {
                 sendText(h, "Task with id " + taskId + " does not exist", 404);
             }
+        } catch (Exception e) {
+            sendText(h, "internal server error", 500);
+            logger.log(Level.SEVERE, "error while delete task", e);
+        }
     }
 
     protected String taskSerialize(Task task) {
